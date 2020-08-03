9c14ca6d3c63961cfedba2356062b085036705c5
NoTeaNoLife <791178881@qq.com>
2020-08-03 16:34:08 +0800

    ......

--- src/main/resources/static/vita/merchant/BackHomePage/js/socket.js
+++ src/main/resources/static/vita/merchant/BackHomePage/js/socket.js
@@ -13,6 +13,8 @@ var SocketModule = {
         }
         // 实现化WebSocket对象，指定要连接的服务器地址与端口  建立连接
         // var socketUrl = "ws://localhost/mSystemMessage/123";
+        socketUrl = "ws://" + SocketModule.getServerAddress() + socketUrl;
+        console.log(socketUrl);
         var socket = new WebSocket(socketUrl);
         //打开事件
         socket.onopen = function () {
@@ -30,6 +32,7 @@ var SocketModule = {
         socket.onerror = function () {
             console.log("websocket发生了错误");
         }
+        // socket.send(msg); // 发送消息到服务端
         return socket;
     },
     printError: function (errorMessage) {
@@ -37,58 +40,14 @@ var SocketModule = {
     },
     onError: function (errorMessage) {
         console.log(errorMessage);
-    }
-}
-
-
-var socket;
-
-function openSocket() {
-    if (typeof (WebSocket) == "undefined") {
-        console.log("您的浏览器不支持WebSocket");
-    } else {
-        console.log("您的浏览器支持WebSocket");
-        //实现化WebSocket对象，指定要连接的服务器地址与端口  建立连接
-        var userId = document.getElementById('userId').value;
-        // var socketUrl="ws://127.0.0.1:22599/webSocket/"+userId;
-        var socketUrl = "ws://localhost/mSystemMessage/123";
-        console.log(socketUrl);
-        if (socket != null) {
-            socket.close();
-            socket = null;
-        }
-        socket = new WebSocket(socketUrl);
-        //打开事件
-        socket.onopen = function () {
-            console.log("websocket已打开");
-            //socket.send("这是来自客户端的消息" + location.href + new Date());
-        };
-        //获得消息事件
-        socket.onmessage = function (msg) {
-            var serverMsg = "收到服务端信息：" + msg.data;
-            console.log(serverMsg);
-            //发现消息进入    开始处理前端触发逻辑
-        };
-        //关闭事件
-        socket.onclose = function () {
-            console.log("websocket已关闭");
-        };
-        //发生了错误事件
-        socket.onerror = function () {
-            console.log("websocket发生了错误");
-        }
-    }
-}
-
-function sendMessage() {
-    if (typeof (WebSocket) == "undefined") {
-        console.log("您的浏览器不支持WebSocket");
-    } else {
-        // console.log("您的浏览器支持WebSocket");
-        var toUserId = document.getElementById('toUserId').value;
-        var contentText = document.getElementById('contentText').value;
-        var msg = '{"toUserId":"' + toUserId + '","contentText":"' + contentText + '"}';
-        console.log(msg);
-        socket.send(msg);
+    },
+    getServerAddress : function() {
+       	var curWwwPath = window.document.location.href;
+      	var pathName = window.document.location.pathname;
+       	var pos = curWwwPath.indexOf(pathName);
+       	var startIndex = curWwwPath.indexOf("//");
+       	var localhostPath = curWwwPath.substring(startIndex+2, pos);
+    	console.log(localhostPath);
+    	return localhostPath
     }
 }
